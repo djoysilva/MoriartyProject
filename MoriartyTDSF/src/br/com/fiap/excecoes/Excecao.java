4681290482d0e3b496f82283a5021015c0093b0d
Joyce Mara da Silva <RM76983@fiap.com.br>
2016-10-18 10:57:24 -0200

    created the exception class

--- MoriartyTDSF/src/br/com/fiap/excecoes/Excecao.java
+++ MoriartyTDSF/src/br/com/fiap/excecoes/Excecao.java
@@ -1,5 +1,13 @@
 package br.com.fiap.excecoes;
 
 public class Excecao extends Exception{
-	oi
+	public Excecao(String msg, Exception e){
+		super(msg, e);
+		e.getMessage();
+		e.printStackTrace();
+	}
+	public Excecao(String msg){
+		super(msg);
+		System.out.println("Erro desconhecido.");
+	}
 }
