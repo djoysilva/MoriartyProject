c3a87966e32c904796d193090d508f5e5d523c47
Joyce Mara da Silva <RM76983@fiap.com.br>
2016-10-19 09:40:25 -0200

    update crud in lancaDespesaDAO

--- MoriartyTDSF/src/br/com/fiap/dao/LancarDespesaDAO.java
+++ MoriartyTDSF/src/br/com/fiap/dao/LancarDespesaDAO.java
@@ -33,5 +33,41 @@ import java.util.List;
             struct.execute();         
             struct.close();
         }
+        
+        public List<LancaDespesa> readList(LancaDespesa lancaDespesa, Connection c) throws Exception{
+            List<LancaDespesa> listDespesa = new ArrayList<LancaDespesa>();
+            PreparedStatement struct = c.prepareStatement("select * from T_AM_ART_LANCA_DESPESA INNER JOIN T_AM_ART_PROCESSO USING (NR_PROCESSO)");
+            ResultSet result = struct.executeQuery();
+            while (result.next()){
+                LancaDespesa lancaDespesa = new LancaDespesa();
+                lancaDespesa.setCodigoLancamento(result.getInt("CD_LANCAMENTO"));
+                lancaDespesa.getCodigoTipoDespesa().setInt(result.getInt("CD_TIPO_DESPESA"));
+                lancaDespesa.getNumero().setInt(result.getInt("NR_PROCESSO"));
+                lancaDespesa.setDataDespesa(result.getString("DT_DESPESA"));
+                lancaDespesa.setValorDespesa(result.getString("VL_DESPESA"));
+                listDespesa.add(lancaDespesa);               
+            }
+            result.close();
+            struct.close();
+            return listDespesa;           
+        }
+        
+        public int update(String data, Double valor, Connection c) throws Exception{
+            PreparedStatement struct = c.prepareStatement("update T_AM_ART_LANCA_DESPESA set DT_DESPESA = ?, VL_DESPESA = ? ");
+            struct.setString(1, data);
+            struct.setDouble(2, valor);
+            int saida = struct.executeQuery();
+            struct.close();
+            return saida;
+             
+        }
+        
+        public void delete(int codigoLanca, Connection c) throws Exception{
+            PreparedStatement struct = c.prepareStatement("delete from T_AM_ART_LANCA_DESPESA where CD_LANCAMENTO = ?");
+            struct.setInt(1, codigoLanca);
+            struct.execute();
+            struct.close();
+        }
+        
     }
     
