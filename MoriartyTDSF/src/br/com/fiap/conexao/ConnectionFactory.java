1a6d598b7f0aab16f6a4a699b5ea0de11c251734
Christian Perrone <RM75949@fiap.com.br>
2016-10-19 10:25:10 -0200

    alteracoes

--- MoriartyTDSF/src/br/com/fiap/conexao/ConnectionFactory.java
+++ MoriartyTDSF/src/br/com/fiap/conexao/ConnectionFactory.java
@@ -6,31 +6,46 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public final class ConnectionFactory {
-	
-   private static ConnectionFactory conexao = null;
-   
-   public static ConnectionFactory controlarInstancia() {
-	   
-	   if (conexao == null) {
-		   conexao = new ConnectionFactory();
-	   }
-	   return conexao;
-   }
-   
-   public Connection getConnection(String usuario, String senha) throws Exception{
-	   
-	   FileReader arquivo = new FileReader(System.getProperty("user.dir") + "/conexao/conexao.txt");
-	   BufferedReader dados = new BufferedReader(arquivo);
-	   
-	   String url = dados.readLine();
-	   
-	   if (url.indexOf("oracle") > 0) {
-		   Class.forName("oracle.jdbc.driver.OracleDriver");
-	   }
-	   
-	   dados.close();
-	   
-	   return DriverManager.getConnection(url, usuario, senha);
-   }
-   
+
+	private static ConnectionFactory conexao = null;
+
+	public static ConnectionFactory controlarInstancia() {
+
+		if (conexao == null) {
+			conexao = new ConnectionFactory();
+		}
+		return conexao;
+	}
+
+	public Connection getConnection(String usuario, String senha) throws Exception{
+
+		FileReader arquivo = new FileReader(System.getProperty("user.dir") + "/conexao/conexao.txt");
+		BufferedReader dados = new BufferedReader(arquivo);
+
+		String url = dados.readLine();
+
+		if (url.indexOf("oracle") > 0) {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}
+
+		dados.close();
+
+		return DriverManager.getConnection(url, usuario, senha);
+	}
+	/*
+	public Connection getConnection() throws Exception{
+
+		FileReader arquivo = new FileReader(System.getProperty("user.dir") + "/conexao/conexao.txt");
+		BufferedReader dados = new BufferedReader(arquivo);
+
+		String url = dados.readLine();
+
+		if (url.indexOf("oracle") > 0) {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		}
+
+		dados.close();
+
+		return DriverManager.getConnection(url);
+	}*/
 }
