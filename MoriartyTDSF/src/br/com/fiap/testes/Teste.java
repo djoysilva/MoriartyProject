e4045737d7182ec4d85be8422d266ec1672e2c6a
Joyce Mara da Silva <RM76983@fiap.com.br>
2016-10-20 11:17:33 -0200

    changed lancaDespesaDAO

--- MoriartyTDSF/src/br/com/fiap/testes/Teste.java
+++ MoriartyTDSF/src/br/com/fiap/testes/Teste.java
@@ -1,10 +1,12 @@
 package br.com.fiap.testes;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.fiap.beans.LancaDespesa;
-import br.com.fiap.beans.LancaHonorario;
 import br.com.fiap.beans.Processo;
+import br.com.fiap.beans.TipoDespesa;
 import br.com.fiap.conexao.ConnectionFactory;
 import br.com.fiap.dao.LancarDespesaDAO;
 import br.com.fiap.dao.ProcessoDAO;
@@ -16,22 +18,40 @@ public class Teste {
 		
 		try {
 			conexao = ConnectionFactory.controlarInstancia().getConnection("", "");
-			System.out.println("deu bom");
+			System.out.println("Conectado!");
 			
 			ProcessoDAO dao = new ProcessoDAO();
-		
 			Processo p =  dao.buscarPorNumeroProcesso(4, conexao);
+			TipoDespesa tipo = new TipoDespesa(1,"Descricao");
+			LancaDespesa ld = new LancaDespesa();
 			
-			LancaDespesa ld = new LancaDespesa(66, 4, 4, "22/06/1988", 666.6, "ojisadsad");
+			ld.setNumero(p.getNumero());
+			ld.setCodigoTipoDespesa(tipo.getCodigo());
+			ld.setDataDespesa("22/06/1988");
+			ld.setValorDespesa(66.6);
+			ld.setDescricao("Descricao");
+			
 			LancarDespesaDAO ldDAO =new LancarDespesaDAO();
-			ldDAO.create(p, 4, conexao);
+			ldDAO.create(ld, conexao);
 			
+			//teste readList
 			
+			List<LancaDespesa> listaLanca = new ArrayList<LancaDespesa>();
+			listaLanca = ldDAO.readList(p.getNumero(), conexao);
+			
+			for(LancaDespesa lista: listaLanca){
+				System.out.println(lista.getCodigoLancamento() + "\n" +
+									lista.getCodigoTipoDespesa() + "\n" +
+									lista.getNumero() + "\n" +
+									lista.getDataDespesa() + "\n" +
+									lista.getValorDespesa() + "\n" +
+									lista.getDescricao());
+			}
 			
 		}
 		catch(Exception e) {
 			e.printStackTrace();
-			System.out.println("deu ruim");
+			System.out.println("Erro.");
 		}
 		try {
 			conexao.close();
